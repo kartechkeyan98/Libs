cf381cef623909bc09746fc52fd0c5177683db96
Kartikeyan Iyer <77189531+kartechkeyan98@users.noreply.github.com>
2024-07-25 14:13:08 +0530

    Update guass_elimination.hpp
    fixed bug for few cases

--- linear_algebra/quantum/resrc/include/algoithms/guass_elimination.hpp
+++ linear_algebra/quantum/resrc/include/algoithms/guass_elimination.hpp
@@ -2,86 +2,91 @@
 #include<matrix/matrix.hpp>
 #include<cmath>
 
+
 template<typename T>
 void guass_elimination(quantum::matrix<T> &A){
-    pair<size_t> shape=A.size();
+    pair<size_t> dims=A.size();
+    double tolerance=1e-6;
 
-    size_t m=shape[0];
-    size_t n=shape[1];
-
-    size_t row=0;
-    for(size_t j=0;j<n;++j){
-        if(row>=m)break;   // nothing left to do
-
-        // find the non-zero value with maximum abs val
-        size_t top=row;
-        T mx=fabs(A(row,j));
-        for(size_t i=row+1;i<m;++i){
-            if(abs(A(i,j))>mx){
-                top=i;
-                mx=abs(A(i,j));
+    for(size_t i=0;i<dims[0];++i){
+        // find the first non-zero column in the submatrix A(i:,:)
+        size_t cidx;
+        size_t rmax;
+        bool discont=true;
+        for(size_t j=i;j<dims[1];++j){
+            rmax=i;
+            T tmp=A(i,j);
+            for(size_t k=i+1;k<dims[1];++k){
+                if(fabs(A(k,j))>fabs(tmp)){
+                    tmp=A(k,j);
+                    rmax=j;
+                }
+            }
+            if(fabs(A(rmax,j))>tolerance){
+                cidx=j;
+                discont=false;
+                break;
             }
         }
-        if(std::round(mx*1000000)/1000000==0){      // this works well till 6 decimal places
-            continue;      // whole column is zero, useless
-        }
-        A.swap_rows(row,top);
+        if(discont)break;
 
-        // now start elminating
-        for(size_t i=row+1;i<m;++i){
-            A.add_multiple_of_row(i,row,-A(i,j)/A(row,j));
+        if(rmax!=i)A.swap_rows(i,rmax);
+
+        for(size_t j=i+1;j<dims[0];++j){
+            A.add_multiple_of_row(j,i,-A(j,cidx)/A(i,cidx));
         }
-        ++row;
     }
-    for(size_t i=0;i<m;++i){
-        for(size_t j=0;j<n;++j){
+    for(size_t i=0;i<dims[0];++i){
+        for(size_t j=0;j<dims[1];++j){
+            // introduces numerical stability later on till 6 digits
+            A(i,j) = T((double)std::round(1e15*(double)A(i,j))/1e15);
             if(A(i,j)==-0.0f)A(i,j)=+0.0f;
         }
     }
-}
+}   // O(m*n^2)
 
 template<typename T>
 void rref(quantum::matrix<T> &A){
-    std::cout<<"Inside rref\n";
-    pair<size_t> shape=A.size();
+    // std::cout<<"Inside rref\n";
+    pair<size_t> dims=A.size();
+    double tolerance=1e-7;
 
-    size_t m=shape[0];
-    size_t n=shape[1];
-
-    size_t row=0;
-    for(size_t j=0;j<n;++j){
-        std::cout<<"Iteration "<<j+1<<std::endl;
-        if(row>=m)break;   // nothing left to do
-
-        // find the non-zero value with maximum abs val
-        size_t top=row;
-        T mx=fabs(A(row,j));
-        for(size_t i=row+1;i<m;++i){
-            if(abs(A(i,j))>mx){
-                top=i;
-                mx=abs(A(i,j));
+    for(size_t i=0;i<dims[0];++i){
+        // find the first non-zero column in the submatrix A(i:,:)
+        size_t cidx;
+        size_t rmax;
+        bool discont=true;
+        for(size_t j=i;j<dims[1];++j){
+            rmax=i;
+            T tmp=A(i,j);
+            for(size_t k=i+1;k<dims[1];++k){
+                if(fabs(A(k,j))>fabs(tmp)){
+                    tmp=A(k,j);
+                    rmax=j;
+                }
+            }
+            if(fabs(A(rmax,j))>tolerance){
+                cidx=j;
+                discont=false;
+                break;
             }
         }
-        if(std::round(mx*1000000)/1000000==0){      // this works well till 6 decimal places
-            continue;      // whole column is zero, useless
-        }
-        A.swap_rows(row,top);
-        std::cout<<A<<std::endl<<std::endl;
+        if(discont)break;
 
-        // now start elminating
-        for(size_t i=0;i<m;++i){
-            if(i==row)continue;
-            A.add_multiple_of_row(i,row,-A(i,j)/A(row,j));
+        if(rmax!=i)A.swap_rows(i,rmax);
+
+        for(size_t j=0;j<dims[0];++j){
+            if(j==i)continue;
+            A.add_multiple_of_row(j,i,-A(j,cidx)/A(i,cidx));
         }
-        A.multiply_row(row,1/A(row,j));
-        ++row;
-        std::cout<<A<<std::endl<<std::endl;
-        
+        A.multiply_row(i,1/A(i,cidx));
     }
-    for(size_t i=0;i<m;++i){
-        for(size_t j=0;j<n;++j){
-            if((float)std::round(A(i,j)*1000000)/1000000==-0.0f)A(i,j)=+0.0f;
+    for(size_t i=0;i<dims[0];++i){
+        for(size_t j=0;j<dims[1];++j){
+            // introduces numerical stability later on till 6 digits
+            A(i,j) = T((double)std::round(1e15*(double)A(i,j))/1e15);
+            if(fabs(A(i,j)-(-0.0f))<=tolerance)A(i,j)=+0.0f;
         }
     }
-    std::cout<<"rref done\n";
-}
+    
+} // O(m*n^2)
